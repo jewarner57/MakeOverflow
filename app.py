c4a49e7aa2be0aac48daafebc1b049a12b43e220
Jonathan Warner <jewarner57@gmail.com>
2020-09-22 16:13:05 -0400

    feat finished login page basic requirements

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, redirect, render_template, url_for, flash
+from flask_login import login_user, logout_user, LoginManager, login_required
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 import os
 from dotenv import load_dotenv
+from user import User
 
 
 ############################################################
@@ -21,6 +23,16 @@ app.secret_key = os.getenv('SECRET_KEY')
 app.config["MONGO_URI"] = "mongodb://localhost:27017/web_final_database"
 mongo = PyMongo(app)
 
+# create login manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    """Loads the current user from the database by id and returns a user object"""
+    return User(mongo.db.users.find_one({"_id": ObjectId(user_id)}))
+
 ############################################################
 # ERROR
 ############################################################
@@ -28,24 +40,19 @@ mongo = PyMongo(app)
 
 @app.errorhandler(404)
 def page_not_found(e):
-    # display a 404 response page
+    """display a 404 response page"""
     return render_template('404.html'), 404
 
+
+@app.errorhandler(401)
+def not_authorized(e):
+    """send the user to the login screen when they try to access a locked page"""
+    flash("Please Login to View This Page")
+    return redirect(url_for("login"))
+
 ############################################################
-# ROUTES
+# AUTH ROUTES
 ############################################################
-
-
-@app.route('/')
-def home():
-    """Display the home page."""
-    # find all users
-    user_data = mongo.db.users.find()
-
-    context = {
-        'users': user_data
-    }
-    return render_template('home.html', **context)
 
 
 @app.route('/signup', methods=["GET", "POST"])
@@ -63,18 +70,19 @@ def signup():
             "name": name,
             "email": email,
             "password": hashed_password,
-            "date_created": date_created
+            "date_created": date_created,
+            "is_authenticated": True
         }
 
+        # check if the email has already been used for an account
         emailExists = mongo.db.users.find_one({"email": email})
-        print(emailExists)
 
         if emailExists:
             flash("Email Is Already Associated with an Account.")
             return redirect(url_for('signup'))
-
-        mongo.db.users.insert_one(new_user)
-        return redirect(url_for('login'))
+        else:
+            mongo.db.users.insert_one(new_user)
+            return redirect(url_for('login'))
     else:
         return render_template('signup.html')
 
@@ -82,10 +90,57 @@ def signup():
 @app.route('/login', methods=["GET", "POST"])
 def login():
     """Display the login page."""
+
     if request.method == 'POST':
-        return render_template('login.html')
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = mongo.db.users.find_one({"email": email})
+
+        # check if user exists and password matches hash
+        if not user or not check_password_hash(user["password"], password):
+            flash("The email or password you entered is invalid")
+            return redirect(url_for('login'))
+
+        userObj = User(user)
+
+        print(userObj)
+
+        login_user(userObj)
+
+        return redirect(url_for('myprofile'))
+
     else:
         return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+############################################################
+# MAIN ROUTES
+############################################################
+
+@app.route('/')
+def home():
+    """Display the home page."""
+    # find all users
+    user_data = mongo.db.users.find()
+
+    context = {
+        'users': user_data
+    }
+    return render_template('home.html', **context)
+
+
+@app.route('/myprofile')
+@login_required
+def myprofile():
+    return render_template('profile.html')
 
 
 if __name__ == '__main__':
---
+++ user.py
@@ -0,0 +1,17 @@
+class User():
+    def __init__(self, user_data):
+        self.name = user_data["name"]
+        self.email = user_data["email"]
+        self.id = str(user_data["_id"])
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.id
