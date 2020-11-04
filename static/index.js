ed682d56d50d9d0d4071446fb12d29d39f633ba3
Jonathan Warner <jewarner57@gmail.com>
2020-11-04 10:19:34 -0500

    redesigned home and signup page with bootstrap

--- static/index.js
+++ static/index.js
@@ -2,4 +2,11 @@ function confirm_delete() {
     return confirm('Are you sure you want to delete?')
 }
 
+<<<<<<< HEAD
 $('#password').tooltip({ 'trigger': 'focus', 'title': 'Password tooltip' });
+=======
+$('input[type=password][name=password]').tooltip({
+    placement: "right",
+    trigger: "focus"
+});
+>>>>>>> d8a3ea3... redesigned home and signup page with bootstrap
